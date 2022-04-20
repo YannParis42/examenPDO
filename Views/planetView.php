10bfa3613acb9a8ac506ffa8e71afd09e50398cd
YannParis42 <yann.paris.42@gmail.com>
2022-04-20 12:49:07 +0200

    commit6

--- Views/planetView.php
+++ Views/planetView.php
@@ -3,15 +3,15 @@
 $title = $planet->getNom();
 //Affiche une card planet
 $content = '
-<div class="card m-2 border border-2 border-success" style="width: 23rem;">
+<div class="card m-2 border border-2 border-success" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">Nom : '. $planet->getNom().'</h5>
     <p class="card-text">Type : '. $planet->getType().'</p>
-    <p class="card-text">Diametre : ' . $planet->getDiametre() . '</p>
-    <p class="card-text">Gravite : ' . $planet->getGravite() . '</p>
-    <p class="card-text">Lien NASA : ' . $planet->getLienNasa() . '</p>
+    <p class="card-text">Diamètre : ' . $planet->getDiametre() . '</p>
+    <p class="card-text">Gravité : ' . $planet->getGravite() . '</p>
     <div class="row">
-      <a href="?" class="btn btn-success col-4">Accueil</a>
+      <a href="' . $planet->getLienNasa() . '" target="_blank" class="btn btn-primary col-8 mb-2 me-2 ">Lien NASA</a>
+      <a href="?" class="btn btn-success col-4 ">Accueil</a>
       <a href="?page=planet" class="btn btn-success col-6 ms-4">Retour liste</a>
     </div>
   </div>
